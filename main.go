b59bf7a3fca8b7e89a2da71e5890aefc6b86c424
Shingo <kawanos@google.com>
2022-01-14 01:01:17 +0900

    implement to notify to Slack temporary

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"time"
 
@@ -18,22 +23,49 @@ func init() {
 	zerolog.TimeFieldFormat = time.RFC3339Nano
 }
 
-var ProjectId = os.Getenv("PROJECT")
-var Subscription = os.Getenv("SUBSCRIPTION")
-var SlackURL = os.Getenv("SLACK_URL")
+var projectId = os.Getenv("PROJECT")
+var subscription = os.Getenv("SUBSCRIPTION")
+var slackUrl = os.Getenv("SLACK_URL")
+
+// to use slack auth parameter
+type SlackStruct struct {
+	SlackUrl     string
+	SlackChannel string
+}
 
 func main() {
-	client, err := pubsub.NewClient(context.Background(), ProjectId)
+	client, err := pubsub.NewClient(context.Background(), projectId)
 	if err != nil {
 		log.Fatal().Err(err)
 	}
-	sub := client.Subscription(Subscription)
+	sub := client.Subscription(subscription)
 	err = sub.Receive(context.Background(), func(ctx context.Context, m *pubsub.Message) {
 		// http.PostForm(SlackURL, )
-		log.Info().Msgf("%+v", m)
+		s := SlackStruct{SlackUrl: slackUrl, SlackChannel: "#log"}
+		s.NotifyToSlack(string(m.Data))
+		log.Info().Msgf("%+v", string(m.Data))
 		m.Ack()
 	})
 	if err != nil {
 		log.Error().Err(err)
+	}
+}
+
+func (s *SlackStruct) NotifyToSlack(message string) error {
+	channel := s.SlackChannel
+	url := s.SlackUrl
+	body, _ := json.Marshal(map[string]string{"text": message, "channel": channel})
+	res, err := http.Post(url, "application/json", bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Error().Msgf("status code error: %d %s", res.StatusCode, res.Status)
+		return errors.New(res.Status)
+	} else {
+		content, _ := ioutil.ReadAll(res.Body)
+		log.Info().Msg(string(content))
+		return nil
 	}
 }
