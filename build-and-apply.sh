d54026fa29820bb5eaa05d0a6eddd08f41989bdf
Shingo <kawanos@google.com>
2022-01-22 13:23:54 +0900

    make it easy to deploy new

---
+++ build-and-apply.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+TAG=$(date '%Y%m%d%H%M')
+gcloud beta builds submit --pack=image=us-central1-docker.pkg.dev/shingo-ar-proto/myrepo/urlmap-notify-worker:$TAG
+envsubst < manifest.yaml | k apply -f -
