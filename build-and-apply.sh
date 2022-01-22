aa74418e8b21d9a8a9c8afa58b854d1921da3ddf
Shingo <kawanos@google.com>
2022-01-22 13:28:28 +0900

    fix it

--- build-and-apply.sh
+++ build-and-apply.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-TAG=$(date '%Y%m%d%H%M')
+export TAG=$(date '+%Y%m%d%H%M')
 gcloud beta builds submit --pack=image=us-central1-docker.pkg.dev/shingo-ar-proto/myrepo/urlmap-notify-worker:$TAG
-envsubst < manifest.yaml | k apply -f -
+envsubst < manifest.yaml | kubectl apply -f -
