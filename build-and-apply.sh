2bff6cf7c2869cecef6ffbc5cd8f7ed979f96ca8
Shingo <kawanos@google.com>
2022-01-23 15:31:08 +0900

    be env variables

--- build-and-apply.sh
+++ build-and-apply.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export TAG=$(date '+%Y%m%d%H%M')
-gcloud beta builds submit --pack=image=us-central1-docker.pkg.dev/shingo-ar-proto/myrepo/urlmap-notify-worker:$TAG
+gcloud beta builds submit --pack=image=us-central1-docker.pkg.dev/$PROJECT/myrepo/urlmap-notify-worker:$TAG
 envsubst < manifest.yaml | kubectl apply -f -
