f6d4ac2e15b1bbc50cc3d199f5f45a8d60ab861a
Shingo <kawanos@google.com>
2022-03-06 19:25:06 +0900

    not deploy when building was failure

--- build-and-apply.sh
+++ build-and-apply.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 export TAG=$(date '+%Y%m%d%H%M')
-gcloud beta builds submit --pack=image=us-central1-docker.pkg.dev/$PROJECT/myrepo/urlmap-notify-worker:$TAG
-envsubst < manifest.yaml | kubectl apply -f -
+if gcloud beta builds submit --pack=image=us-central1-docker.pkg.dev/$PROJECT/myrepo/urlmap-notify-worker:$TAG;
+then
+    envsubst < manifest.yaml | kubectl apply -f -
+fi
